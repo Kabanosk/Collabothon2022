3d717c8acd18ab675e7faebc0e0f48a902d59c8e
pawel-kojma <pawel.kojma@hotmail.com>
2022-07-13 18:42:51 +0200

    kox ui frontend

--- lib/main_view_un.dart
+++ lib/main_view_un.dart
@@ -13,6 +13,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
+List<String> selectedItems = [];
+
 class MainView extends StatefulWidget {
   const MainView({Key? key}) : super(key: key);
 
@@ -21,6 +23,37 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
+  List<String> _selectedItems = [];
+
+  void _showMultiSelect() async {
+    // a list of selectable items
+    // these items can be hard-coded or dynamically fetched from a database/API
+    final List<String> _items = [
+      'Flutter',
+      'Node.js',
+      'React Native',
+      'Java',
+      'Docker',
+      'MySQL'
+    ];
+
+    final List<String>? results = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return MultiSelect(items: _items);
+      },
+    );
+
+    // Update UI
+    if (results != null) {
+      setState(() {
+        _selectedItems = results;
+        selectedItems = results;
+      });
+    }
+    print(results);
+  }
+
   @override
   Widget build(BuildContext context) {
     List<String> categories = <String>[
@@ -68,12 +101,75 @@ class _MainViewState extends State<MainView> {
               ),
               trailing: const Icon(
                 Icons.add,
-                color: Colors.red,
+                color: Colors.blueAccent,
                 semanticLabel: 'More',
               ),
-              onTap: null);
+              onTap: _showMultiSelect);
         },
       ),
+    );
+  }
+}
+
+class MultiSelect extends StatefulWidget {
+  final List<String> items;
+  const MultiSelect({Key? key, required this.items}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _MultiSelectState();
+}
+
+class _MultiSelectState extends State<MultiSelect> {
+  // this variable holds the selected items
+  final List<String> _selectedItems = [];
+
+// This function is triggered when a checkbox is checked or unchecked
+  void _itemChange(String itemValue, bool isSelected) {
+    setState(() {
+      if (isSelected) {
+        _selectedItems.add(itemValue);
+      } else {
+        _selectedItems.remove(itemValue);
+      }
+    });
+  }
+
+  // this function is called when the Cancel button is pressed
+  void _cancel() {
+    Navigator.pop(context);
+  }
+
+// this function is called when the Submit button is tapped
+  void _submit() {
+    Navigator.pop(context, _selectedItems);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Select Topics'),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: widget.items
+              .map((item) => CheckboxListTile(
+                    value: _selectedItems.contains(item),
+                    title: Text(item),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    onChanged: (isChecked) => _itemChange(item, isChecked!),
+                  ))
+              .toList(),
+        ),
+      ),
+      actions: [
+        TextButton(
+          child: const Text('Cancel'),
+          onPressed: _cancel,
+        ),
+        ElevatedButton(
+          child: const Text('Submit'),
+          onPressed: _submit,
+        ),
+      ],
     );
   }
 }
